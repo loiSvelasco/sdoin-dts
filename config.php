98f33316d7bc5a8e14df551c0e70204a8b7dc78b
Louis Velasco <loisuperficialvelasco@gmail.com>
2022-04-06 20:50:35 +0800

    Removed unused definitions in config

--- config.php
+++ config.php
@@ -3,9 +3,6 @@
 session_start();
 // session_destroy();
 
-defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
-// defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "admin/img");
-
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 defined("DB_USER") ? null : define("DB_USER", "root");
 defined("DB_PASS") ? null : define("DB_PASS", "");
