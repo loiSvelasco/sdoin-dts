193c504661a636568e582f11c8ed94bad2e47af0
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-05-06 08:36:06 +0800

    fix nav links

--- dashboard/index.php
+++ dashboard/index.php
@@ -20,108 +20,156 @@ if(isset($_SESSION['timestamp']))
     }
 }
 
-if($_SERVER['REQUEST_URI'] == "/sdoin-dts/dashboard/") {
+if ($_SERVER['REQUEST_URI'] == "/sdoin-dts/dashboard/") 
+{
     include("includes/documents.php");
-} else if (isset($_GET['print'])) {
+}
+
+else if (isset($_GET['print'])) 
+{
     redirect("print/barcode.php?tracking=" . escape_string($_GET['print']));
-} else if (isset($_GET['view']) && isset($_GET['file'])) {
+}
+
+else if (isset($_GET['view']) && isset($_GET['file'])) 
+{
     redirect("uploads/".escape_string($_GET['file']));
-} else if (isset($_GET['manipulate'])) {
+}
+
+else if (isset($_GET['manipulate']))
+{
     include("actions/manipulate.php");
-} else if (isset($_GET['tracking'])) {
+}
+
+else if (isset($_GET['tracking']))
+{
     include("includes/tracking.php");
-} else if (isset($_GET['logout'])) {
+}
+
+else if (isset($_GET['logout']))
+{
     include("actions/logout.php");
-} else if (isset($_GET['editDoc'])) {
+}
+
+else if (isset($_GET['editDoc']))
+{
     include("includes/editDoc.php");
 }
- // if needed, add more else ifs here
+
+else if (isset($_GET['documents']))
+{
+    include("includes/documents.php");
+}
+
+else if (isset($_GET['upload']))
+{
+    if($_SESSION['role'] == 2 || 
+    $_SESSION['role'] == 1 || 
+    $_SESSION['unit'] == 117 || 
+    $_SESSION['unit'] == 127 || 
+    $_SESSION['unit'] == 128)
+    {   
+        include("includes/upload.php");   
+    } else {
+        set_message_alert("alert-warning", "fa-exclamation", "Access Denied.");
+        redirect("./?documents");
+    }
+}
+
+else if (isset($_GET['debug']))
+{
+    if($_SESSION['role'] == 1) {
+        include("includes/debug.php");
+    } else {
+        set_message_alert("alert-warning", "fa-exclamation", "Access Denied.");
+        redirect("./?documents");
+    }
+}
+
+else if (isset($_GET['admin']))
+{
+    if($_SESSION['role'] == 1)
+    {
+        include("includes/admin.php");
+    }
+    else {
+        set_message_alert("alert-warning", "fa-exclamation", "Access Denied.");
+        redirect("./?documents");
+    }
+}
+
+else if (isset($_GET['allReceived']))
+{
+    if($_SESSION['role'] != 1)
+        redirect("./?documents");
+    include("includes/allReceived.php");
+}
+
+else if (isset($_GET['allReleased']))
+{
+    if($_SESSION['role'] != 1)
+        redirect("./?documents");
+    include("includes/allReleased.php");
+}
+
+else if (isset($_GET['allDocs']))
+{
+    if($_SESSION['role'] != 1)
+        redirect("./?documents");
+    include("includes/allDocs.php");
+}
+
+else if (isset($_GET['lapsedDocs']))
+{
+    if($_SESSION['role'] != 1)
+        redirect("./?documents");
+    include("includes/lapsedDocs.php");
+}
+
+else if (isset($_GET['users']))
+{
+    if($_SESSION['role'] != 1)
+        redirect("./?documents");
+    include("includes/users.php");
+}
+
+else if (isset($_GET['reports']))
+{
+    include("includes/reports.php");
+}
+
+else if (isset($_GET['received']))
+{
+    include("includes/received.php");
+}
+
+else if (isset($_GET['released']))
+{
+    include("includes/released.php");
+}
+
+else if (isset($_GET['accomplished']))
+{
+    include("includes/accomplished.php");
+}
+
+else if (isset($_GET['profile']))
+{
+    include("includes/profile.php");
+}
+
+else if (isset($_GET['scanrec']))
+{
+    include("includes/scanrec.php");
+}
+
+else if (isset($_GET['404']))
+{
+    include("includes/404.php");
+}
+
 else 
 {
-    switch($_SERVER['QUERY_STRING'])
-    {
-        case 'documents':
-            include("includes/documents.php");
-            break;
-        case 'upload':
-            if($_SESSION['role'] == 2 || 
-            $_SESSION['role'] == 1 || 
-            $_SESSION['unit'] == 117 || 
-            $_SESSION['unit'] == 127 || 
-            $_SESSION['unit'] == 128)
-            {   
-                include("includes/upload.php");   
-            } else {
-                set_message_alert("alert-warning", "fa-exclamation", "Access Denied.");
-                redirect("./?documents");
-            }
-            break;
-        case 'debug':
-            if($_SESSION['role'] == 1) {
-                include("includes/debug.php");
-            } else {
-                set_message_alert("alert-warning", "fa-exclamation", "Access Denied.");
-                redirect("./?documents");
-            }
-            break;
-        case 'admin':
-            if($_SESSION['role'] == 1) {
-                include("includes/admin.php");
-            } else {
-                set_message_alert("alert-warning", "fa-exclamation", "Access Denied.");
-                redirect("./?documents");
-            }
-            break;
-        case 'allReceived':
-            if($_SESSION['role'] != 1)
-                redirect("./?documents");
-            include("includes/allReceived.php");
-            break;
-        case 'allReleased':
-            if($_SESSION['role'] != 1)
-                redirect("./?documents");
-            include("includes/allReleased.php");
-            break;
-        case 'allDocs':
-            if($_SESSION['role'] != 1)
-                redirect("./?documents");
-            include("includes/allDocs.php");
-            break;
-        case 'lapsedDocs':
-            if($_SESSION['role'] != 1)
-                redirect("./?documents");
-            include("includes/lapsedDocs.php");
-            break;
-        case 'users':
-            if($_SESSION['role'] != 1)
-                redirect("./?documents");
-            include("includes/users.php");
-            break;
-        case 'reports':
-            include("includes/reports.php");
-            break;
-        case 'received':
-            include("includes/received.php");
-            break;
-        case 'released':
-            include("includes/released.php");
-            break;
-        case 'accomplished':
-            include("includes/accomplished.php");
-            break;
-        case 'profile':
-            include("includes/profile.php");
-            break;
-        case 'scanrec':
-            include("includes/scanrec.php");
-            break;
-        case '404':
-            include("includes/404.php");
-            break;
-        default:
-            redirect("./?404");
-            break;
-    }
+    redirect("./?404");
 }
 
 include("includes/modals.php");
