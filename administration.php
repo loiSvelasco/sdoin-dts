e5ddb1e9d04325de4f36501aae06aaf19e648e63
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-08-10 10:51:05 +0800

    Fixed merging issue

--- administration.php
+++ administration.php
@@ -102,7 +102,7 @@ function allUsers()
 {
     $morningLookElla = query("SELECT * FROM users, user_details WHERE users.id = user_details.id");
     love($morningLookElla);
-
+    $counter = 0;
     while($row = fetch_array($morningLookElla))
     {
         $id = $row['id'];
