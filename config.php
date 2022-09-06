82d7d5b1cb71313a7f19ce46b2a2cf9514ee2b50
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-09-06 21:37:51 +0800

    error reporting

--- config.php
+++ config.php
@@ -6,13 +6,13 @@
   $DOSignUp = true;
   $fieldSignUp = true;
 
-  if ($staging)
-  {
+  // if ($staging)
+  // {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-  }
+  // }
 
   include 'kint.phar';
   Kint::$aliases[] = 'dd';
