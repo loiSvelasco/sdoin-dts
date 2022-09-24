18c384cd033abf6dd2f996ed67b5783ea8ebc5d0
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-09-24 14:08:30 +0800

    for deployment

--- dashboard/includes/scanrec.php
+++ dashboard/includes/scanrec.php
@@ -30,32 +30,35 @@
         </div>
       </div>
 
-      <div class="col-12">
-        <div class="card">
-          <div class="card-header bg-white">
+      <div class="col-lg-12">
+        <div class="card rounded-0">
+          <div class="card-header border-info bg-white">
             <h3 class="card-title">
-            <button type="button" class="btn btn-sm btn-flat btn-info" data-toggle="modal" data-target="#modal-release-multi-doc" form="release" name="rel-selected">Release Selected</button>
+            <form action="actions/multi-release.php?refer=<?php echo $_SERVER['REQUEST_URI']; ?>" id="release" method="post" onsubmit="relDocMultiBtn.hidden = true; document.getElementById('relDocMultiBtn2').style.display = '';"></form>
+            <button type="button" class="btn btn-flat btn-info" data-toggle="modal" data-target="#modal-release-multi-doc" form="release" name="rel-selected">
+            <i class="fa fa-file-export"></i>&nbsp;&nbsp;&nbsp;
+            Release Selected</button>
             &nbsp;&nbsp;&nbsp;Documents to Release</h3>
           </div>
-          <!-- /.card-header -->
           <div class="card-body bg-white">
             <div class="table-responsive-sm">
-              <table id="releaseTable" class="table table-striped table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th class="text-center col-1"><form action="actions/multi-release.php?refer=<?php echo $_SERVER['REQUEST_URI']; ?>" id="release" method="post">
-                      <input type="checkbox" id="select-all-rel" data-toggle="tooltip" data-placement="left" title="Select All">&nbsp;&nbsp;</form></th>
-                  <th class="text-center col-1">Tracking</th>
-                  <th class="text-center">for</th>
-                  <th class="text-center col-8">Title</th>
-                  <th class="text-center col-1">Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <!-- <?php // echo get_to_release(); ?> -->
-                </tbody>
-              </table>
-            </div><!-- /.card-body -->
+                <table id="releaseTable" class="table table-striped table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th class="text-center col-1">
+                      <input type="checkbox" id="select-all-rel" data-toggle="tooltip" data-placement="left" title="Select All">
+                    </th>
+                    <th class="text-center col-2">Tracking</th>
+                    <th class="text-center col-2">for</th>
+                    <th class="text-center col-7">Title</th>
+                    <th class="text-center col">Action</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                      <!-- <?php //echo get_to_release(); ?> -->
+                  </tbody>
+                </table>
+            </div>
           </div>
         </div>
       </div>
